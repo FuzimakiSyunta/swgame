9e189e329ea2189975f21ae3c4db9db93bfcfa25
FuzimakiSyunta <k022g0033@g.neec.ac.jp>
2025-08-28 00:40:16 +0900

    feat: add ranking endpoint

--- api/ranking.ts
+++ api/ranking.ts
@@ -1,18 +1,13 @@
-// api/ranking.ts
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export default async function handler(req: any, res: any) {
-  try {
-    const scores = await prisma.score.findMany({
-      orderBy: { score: "desc" },
-      take: 5,
-    });
+  const scores = await prisma.score.findMany({
+    orderBy: { score: "desc" },
+    take: 5,
+  });
 
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).send(scores);
-  } catch (err: any) {
-    res.status(500).send({ error: err.message });
-  }
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).json(scores);
 }

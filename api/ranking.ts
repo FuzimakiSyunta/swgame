9fbb7f7fb97215e516a76779c4e64e7c7a826ee1
FuzimakiSyunta <k022g0033@g.neec.ac.jp>
2025-08-28 00:47:02 +0900

    fix: simple ranking API for vercel

--- api/ranking.ts
+++ api/ranking.ts
@@ -2,12 +2,11 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export default async function handler(req: any, res: any) {
+export default async (req, res) => {
   const scores = await prisma.score.findMany({
     orderBy: { score: "desc" },
     take: 5,
   });
 
-  res.setHeader("Content-Type", "application/json");
   res.status(200).json(scores);
-}
+};
